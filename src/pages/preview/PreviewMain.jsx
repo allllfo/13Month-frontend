df0829a0fa1aca62c65a734d8cb68e25a433c107
NOEL-code <junusong12@hanyang.ac.kr>
2024-03-13 14:16:53 +0900

    feat: 돋보기 애니메이션 적용

---
+++ src/pages/preview/PreviewMain.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import lenseImg from "~/assets/lense.png";
+import "./PreviewMain.css";
+import style from "./PreviewMain.module.css";
+
+const PreviewMain = () => {
+  return (
+    <div className="bg-white h-screen p-4">
+      <div className="flex flex-col items-center mt-28">
+        <div>
+          <p className="text-center text-4xl font-bold">연말정산 미리보기</p>
+        </div>
+
+        <div className="mt-16">
+          {/* Hover scale effect added here */}
+          <img
+            src={lenseImg}
+            alt="Magnifying glass"
+            // className="w-36 h-36 transition-transform duration-300 ease-in-out hover:scale-150 img3"
+            className={style.img3}
+          />
+        </div>
+        <div className="text-center mt-5">
+          <p className="text-xl font-semibold mt-4">
+            13월의 강도에서 13일의 월급으로
+          </p>
+          <p className="text-xl font-semibold mt-4">연말정산</p>
+          <p className="text-xl font-semibold mt-4">미리미리 준비하세요!</p>
+        </div>
+        <div className="flex flex-col items-center justify-center mt-10 w-full">
+          <div className="w-9/12">
+            <button className="bg-blue-500 text-white text-lg w-full h-12 py-2 rounded-full font-semibold mb-4">
+              연말정산 계산하기
+            </button>
+          </div>
+          <div className="w-9/12">
+            <button className="bg-blue-500 text-white text-lg w-full h-12 py-2 rounded-full font-semibold">
+              이전결과 확인하기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PreviewMain;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+<<<<<<< Updated upstream
 import FindOutPage from "~/pages/products/findout/page";
 import IndexPage from "~/components/Main/IndexPage";
 import Login from "~/components/Main/Login";
@@ -8,6 +9,18 @@ import EasyETF from "~/components/EasySeries/EasyETF";
 import EasyFound from "~/components/EasySeries/EasyFound";
 import EasyMain from "~/components/EasySeries/EasyMain";
 import EasyTax from "~/components/EasySeries/EasyTax";
+=======
+import FindOutPage from "../pages/products/findout/page";
+import IndexPage from "../pages/Main/IndexPage";
+import Login from "../pages/Main/Login";
+import MainPage from "../pages/Main/MainPage";
+import MyPage from "../pages/Main/MyPage";
+import EasyETF from "../pages/EasySeries/EasyETF";
+import EasyFound from "../pages/EasySeries/EasyFound";
+import EasyMain from "../pages/EasySeries/EasyMain";
+import EasyTax from "../pages/EasySeries/EasyTax";
+import PreviewMain from "../pages/preview/PreviewMain";
+>>>>>>> Stashed changes
 
 export const mainRoutes = [
   {
