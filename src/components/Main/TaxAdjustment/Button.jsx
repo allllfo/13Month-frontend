4467f0103ba77b4354b1e8f9a5c0daf8f9ddbc5b
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-03-14 16:34:59 +0900

    feat: MainPage 버튼 위치 구성

--- src/components/Main/TaxAdjustment/Button.jsx
+++ src/components/Main/TaxAdjustment/Button.jsx
@@ -5,7 +5,10 @@ export default function Button(props) {
   const subTitle = props.subTitle;
 
   return (
-    <div className="">
+    <div
+      className="bg-white border rounded-xl"
+      style={{ width: '100%', height: '100%' }}
+    >
       <p>{title}</p>
       <p>{subTitle}</p>
     </div>
--- src/components/Main/TaxAdjustment/TaxAdjustment.jsx
+++ src/components/Main/TaxAdjustment/TaxAdjustment.jsx
@@ -4,20 +4,24 @@ import Button from './Button';
 
 export default function TaxAdjustment() {
   return (
-    <div>
-      <div className="w-1/1 bg-blue-700" style={{ height: '200px' }}>
-        {' '}
+    <div className="mt-10">
+      <div className="flex items-center justify-start gap-2 mb-1">
+        <p className="text-xl font-bold">돈 버는 서비스</p>
+        <img className="h-10" src="/src/assets/images/coin.png"></img>
+      </div>
+
+      <div className="w-1/1 bg-blue-700 mb-4" style={{ height: '180px' }}>
         <Button title="testTItle" subTitle="testSUb"></Button>
       </div>
 
-      <div className="w-1/2 bg-red-700" style={{ height: '200px' }}>
-        {' '}
-        <Button title="testTItle" subTitle="testSUb"></Button>
-      </div>
+      <div className="flex gap-2">
+        <div className="w-1/2 bg-red-700" style={{ height: '140px' }}>
+          <Button title="testTItle" subTitle="testSUb"></Button>
+        </div>
 
-      <div className="w-1/2 bg-red-200" style={{ height: '200px' }}>
-        {' '}
-        <Button title="testTItle" subTitle="testSUb"></Button>
+        <div className="w-1/2 bg-red-200" style={{ height: '140px' }}>
+          <Button title="testTItle" subTitle="testSUb"></Button>
+        </div>
       </div>
     </div>
   );
