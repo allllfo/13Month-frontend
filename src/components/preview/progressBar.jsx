e7e09875f5509f191a455409ae80575c46037839
Future <86925185+allllfo@users.noreply.github.com>
2024-03-15 08:26:37 +0900

    feat: 카드 공제 컴포넌트 추가 (#13)
    * feat:PreviewLoading 페이지 추가
    
    * chore : eslintrc.cjs 속성 추가
    
    * feat: ladingComments 컴포넌트 추가
    
    * feat: LoadingPreview 페이지 완성
    
    * chore: eslintrc.cjs 속성 추가
    
    * feat: PreviewMain페이지에 로딩페이지로 링크 추가
    
    * refactor: router 위치 변경 및 수정
    
    * comment: PreviewLoading페이지 불필요한 주석 삭제
    
    * style: PreviewLoading페이지 요소 위치 변경
    
    * feat(WIP) : 카드공제 컴포넌트 생성
    
    * feat(WIP): card accordion 생성하기
    
    * feat: 카드 공제 컴포넌트 완성
    
    * rename:상대경로를  절대경로로 변경

---
+++ src/components/preview/progressBar.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+
+function ProgressBar({ percentage, color, isAnimation }) {
+  const [currentPercentage, setCurrentPercentage] = useState(0);
+
+  useEffect(() => {
+    if (isAnimation) {
+      const interval = setInterval(() => {
+        if (currentPercentage < percentage) {
+          setCurrentPercentage((prevPercentage) =>
+            Math.min(prevPercentage + 1, percentage)
+          );
+        } else {
+          clearInterval(interval);
+        }
+      }, 10);
+
+      return () => clearInterval(interval);
+    }
+  }, [currentPercentage, percentage, isAnimation]);
+
+  const progressBarStyle = {
+    width: `${currentPercentage}%`,
+    backgroundColor: color,
+    color: "black",
+    transition: "width 0.2s ease", // 너비 변화에 대한 애니메이션 추가
+  };
+
+  return (
+    <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+      <div
+        className="bg-blue-600 text-xl font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+        style={progressBarStyle}
+      >
+        {currentPercentage}원
+      </div>
+    </div>
+  );
+}
+
+export default ProgressBar;
