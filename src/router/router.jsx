c7b6b551d9a09db023df3eece04fc6665ba488c6
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2024-03-14 11:29:09 +0900

    fix: 삭제된 페이지들 import에서 제거 (#11)
    * refactor: 메인 레이아웃 추가
    
    * chore: jsconfig.json 추
    
    * design: Pretendard 서체 적용
    
    * feat(wip): PreviewResult 페이지 추가
    
    작업중
    
    * style: js 스타일 속성으로 변경
    
    * chore: flowbite-react 설치 및 설정
    
    * design: 메인 레이아웃 넓이 수정
    
    * feat: PreviewResult 페이지 개발
    
    * fix: 삭제된 페이지들 import에서 제거

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,14 +1,8 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import FindOutPage from "~/pages/products/findout/page";
-import IndexPage from "~/pages/Main/IndexPage";
-import LoginPage from '~/pages/LoginPage/LoginPage';
-import MainPage from '~/pages/MainPage/MainPage';
-import MyPage from "~/pages/Main/MyPage";
-import EasyETF from "~/pages/EasySeries/EasyETF";
-import EasyFound from "~/pages/EasySeries/EasyFound";
-import EasyMain from "~/pages/EasySeries/EasyMain";
-import EasyTax from "~/pages/EasySeries/EasyTax";
+import LoginPage from "~/pages/LoginPage/LoginPage";
+import MainPage from "~/pages/MainPage/MainPage";
 import PreviewMain from "~/pages/preview/PreviewMain";
 import MainLayout from "~/pages/layout";
 import HousingFundLoan from "../components/HousingFundLoan";
@@ -22,31 +16,11 @@ export const mainRoutes = [
     children: [
       {
         path: "/",
-        element: <Login />,
+        element: <LoginPage />,
       },
       {
         path: "/main",
         element: <MainPage />,
-      },
-      {
-        path: "/mypage",
-        element: <MyPage />,
-      },
-      {
-        path: "/EasyETF",
-        element: <EasyETF />,
-      },
-      {
-        path: "/EasyFound",
-        element: <EasyFound />,
-      },
-      {
-        path: "/EasyMain",
-        element: <EasyMain />,
-      },
-      {
-        path: "/EasyTax",
-        element: <EasyTax />,
       },
       {
         path: "/product/findout",
