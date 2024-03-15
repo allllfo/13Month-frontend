048de54d609b96564da96c3e9e60422e98c97880
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-03-15 09:06:00 +0900

    feat: 버튼 안 텍스트 및 아이콘 배치

--- src/components/Main/TaxAdjustment/Button.jsx
+++ src/components/Main/TaxAdjustment/Button.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 
 export default function Button(props) {
-  const title = props.title;
-  const subTitle = props.subTitle;
+  const firstOfTitle = props.firstOfTitle;
+  const secondOfTitle = props.secondOfTitle;
+
+  const firstOfSubTitle = props.firstOfSubTitle;
+  const secondOfSubTitle = props.secondOfSubTitle;
+
+  const imageSrc = props.imageSrc;
 
   return (
     <div
-      className="bg-white border rounded-xl"
+      className="bg-white border rounded-xl p-4 relative"
       style={{ width: '100%', height: '100%' }}
     >
-      <p>{title}</p>
-      <p>{subTitle}</p>
+      <p className="text-xl font-bold mb-2">
+        {firstOfTitle}
+        <br></br>
+        {secondOfTitle}
+      </p>
+
+      <p className="text-medium text-slate-500">
+        {firstOfSubTitle}
+        <br></br>
+        {secondOfSubTitle}
+      </p>
+
+      <div className="absolute right-3 bottom-3">
+        <img className="h-8" src={imageSrc}></img>
+      </div>
     </div>
   );
 }
