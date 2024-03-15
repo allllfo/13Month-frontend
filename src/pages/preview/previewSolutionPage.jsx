88572383de1a80ba8529b5f4f7057ab62ab2d218
allllfo <mirae4292@gmail.com>
2024-03-15 15:02:46 +0900

    temp: dev 임시 보관

--- src/pages/preview/previewSolutionPage.jsx
+++ src/pages/preview/previewSolutionPage.jsx
@@ -3,7 +3,7 @@ import CardComponent from "~/components/Preview/Card";
 import HousingFundLoan from "~/components/Preview/HousingFundLoan";
 import PersonComponent from "~/components/Preview/Person";
 import SmallBusiness from "~/components/Preview/SmallBusiness";
-import MonthlyRental from "~/components/Preview/MonthlyRental";
+import MonthlyRental from "~/components/Preview/monthlyRental";
 import PendingAndIRP from "~/components/Preview/PendingAndIRP";
 
 export default function PreviewSolutionPage() {
