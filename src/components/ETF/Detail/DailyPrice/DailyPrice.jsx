d1c1f63765e82b17ced11dee971366e72fe171b0
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-03-19 10:47:32 +0900

    chore: 디테일 페이지 컴포넌트 생성

---
+++ src/components/ETF/Detail/DailyPrice/DailyPrice.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DailyPrice() {
+  return <div>DailyPrice</div>;
+}
--- src/pages/ETF/etfDetailPage.jsx
+++ src/pages/ETF/etfDetailPage.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import { useLocation } from "react-router";
 
 import TopBackBar from "~/components/TopBackBar/TopBackBar";
+import CommonInfo from "~/components/ETF/Detail/CommonInfo";
 
-export default function etfDetailPage() {
-  const location = useLocation();
-  console.log("location: ", location);
+import Chart from "~/components/ETF/Detail/Chart/Chart";
+import DailyPrice from "~/components/ETF/Detail/DailyPrice/DailyPrice";
+import DetailInfo from "~/components/ETF/Detail/DetailInfo/DetailInfo";
+import Community from "~/components/ETF/Detail/Community/Community";
+
+export default function etfDetailPage(props) {
+  // const code = props.code;
+
+  // test
+  const code = 47531;
 
   return (
     <div>
