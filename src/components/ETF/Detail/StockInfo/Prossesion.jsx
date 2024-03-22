33a39c02ec5901373e6b36a8e20c781c63876ef3
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-03-22 10:42:15 +0900

    fix: etf 상세페이지 보유비중 차트 스타일 수정

--- src/components/ETF/Detail/StockInfo/Prossesion.jsx
+++ src/components/ETF/Detail/StockInfo/Prossesion.jsx
@@ -21,7 +21,7 @@ export default function Prossesion(props) {
     <div className="mb-16">
       <p className="font-bold text-2xl">보유비중 TOP 10</p>
 
-      <div className="w-full h-96">
+      <div className="w-full" style={{ height: "440px" }}>
         <ResponsivePie
           data={data}
           tooltip={(point) => {
@@ -33,14 +33,21 @@ export default function Prossesion(props) {
                 className="bg-white border rounded-md text-xs p-1 text-center"
                 style={{ borderColor: color }}
               >
-                <p className="text-xs">{id}</p>
-                <p className="text-sm font-bold">{value}%</p>
+                <p className="text-xs font-semibold">{id}</p>
+                <div className="flex justify-center">
+                  <div
+                    className="w-3 h-3 mt-1 rounded-full bg-black mr-1"
+                    style={{ backgroundColor: color }}
+                  ></div>
+
+                  <p className="text-sm font-bold">{value}%</p>
+                </div>
               </div>
             );
           }}
           arcLabel={(item) => `${item.value}%`}
-          margin={{ top: 20, right: 70, bottom: 80, left: 70 }}
-          innerRadius={0.5}
+          margin={{ top: 20, right: 80, bottom: 80, left: 80 }}
+          innerRadius={0.4}
           padAngle={0.7}
           cornerRadius={3}
           activeOuterRadiusOffset={8}
@@ -50,43 +57,19 @@ export default function Prossesion(props) {
             from: "color",
             modifiers: [["darker", 0.2]],
           }}
-          arcLinkLabelsStraightLength={6}
-          arcLinkLabelsSkipAngle={10}
+          arcLinkLabelsStraightLength={10}
+          arcLinkLabelsSkipAngle={14}
           arcLinkLabelsTextColor="#333333"
           arcLinkLabelsThickness={2}
           arcLinkLabelsColor={{ from: "color" }}
-          arcLabelsSkipAngle={10}
-          arcLinkLabelsDiagonalLength={6}
+          arcLabelsSkipAngle={16}
+          arcLinkLabelsDiagonalLength={12}
           arcLinkLabelsTextOffset={2}
           arcLabelsTextColor={{
             from: "color",
             modifiers: [["darker", 2]],
           }}
-          legends={[
-            {
-              anchor: "bottom",
-              direction: "row",
-              justify: false,
-              translateX: 0,
-              translateY: 56,
-              itemsSpacing: 0,
-              itemWidth: 100,
-              itemHeight: 18,
-              itemTextColor: "#999",
-              itemDirection: "left-to-right",
-              itemOpacity: 1,
-              symbolSize: 18,
-              symbolShape: "circle",
-              effects: [
-                {
-                  on: "hover",
-                  style: {
-                    itemTextColor: "#000",
-                  },
-                },
-              ],
-            },
-          ]}
+          legends={[]}
         />
       </div>
     </div>
