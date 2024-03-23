f0b00ef2dcd80b292db324388263d3accbd7943e
Future <86925185+allllfo@users.noreply.github.com>
2024-03-24 00:46:34 +0900

    Feature/45 solution page - 인적공제만 (#53)
    * feat: 인적 공제 추가
    
    * refactor: 인적 공제 마무리

--- src/pages/preview/previewSolutionPage.jsx
+++ src/pages/preview/previewSolutionPage.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CardComponent from "~/components/Preview/Card";
 import HousingFundLoan from "~/components/Preview/HousingFundLoan";
 import PersonComponent from "~/components/Preview/Person";
 import SmallBusiness from "~/components/Preview/SmallBusiness";
 import MonthlyRental from "~/components/Preview/MonthlyRental";
 import PendingAndIRP from "~/components/Preview/PendingAndIRP";
+import MonthAndHouse from "~/components/Preview/MonthAndHouse";
+import { useSearchParams } from "react-router-dom";
 import BlueButton from "~/components/BlueButton/BlueButton";
 
 export default function PreviewSolutionPage() {
+  const [total, setTotal] = React.useState({
+    person: 0,
+    house: 0,
+    business: 0,
+  });
+  const [result, setResult] = React.useState(0);
+
+  function updateTotal(type, value) {
+    setTotal((prevTotal) => ({
+      ...prevTotal,
+      [type]: value,
+    }));
+  }
+
+  function calculateTotal() {
+    const temp = total.person + total.business + total.house;
+    setResult(temp);
+  }
+
+  useEffect(() => {
+    // totalPeopleNum이 변경될 때마다 totalPrice를 업데이트합니다.
+    calculateTotal();
+  }, [total, calculateTotal]);
+
   return (
     <>
       <div className="flex flex-col items-center">
-        <h1 className="h3 mb-1 mt-6">
-          솔루션 이행시 최대 0원까지 아낄 수 있어요!
+        <h1 className="text-xl font-bold mb-1 mt-6">
+          솔루션 이행시 최대 {result}원까지 아낄 수 있어요!
         </h1>
         <div className="text-center">
           <p className="mediumGreyText mb-3 text-base">
@@ -22,14 +48,13 @@ export default function PreviewSolutionPage() {
         </div>
       </div>
       <CardComponent />
-      <PersonComponent />
-      <MonthlyRental />
-      <HousingFundLoan />
-      <SmallBusiness />
-      <PendingAndIRP />
-      <div className="flex justify-center mb-5">
-        <BlueButton text="결과 확인하기" destination="/preview/result/detail" />
-      </div>
+      <PersonComponent updateTotal={updateTotal} />
+      <MonthAndHouse updateTotal={updateTotal} />
+      <SmallBusiness updateTotal={updateTotal} />
+      <PendingAndIRP updateTotal={updateTotal} />
+      <button className="bg-blue-500 text-white text-lg w-full h-20 rounded-lg font-semibold">
+        결과 확인하기
+      </button>
     </>
   );
 }
