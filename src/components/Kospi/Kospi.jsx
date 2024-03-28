919f524a119ca7a15c4afb77dcd14ff8a4a299a4
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-03-28 16:16:25 +0900

    feat: Kospi 컴포넌트 구현

--- src/components/Kospi/Kospi.jsx
+++ src/components/Kospi/Kospi.jsx
@@ -6,6 +6,8 @@ export default function Kospi(props) {
   const currentPeriod = props.currentPeriod;
 
   const [kospi, setKospi] = useState();
+  const [message, setMessage] = useState();
+  const [style, setStyle] = useState("");
 
   useEffect(() => {
     const resp = getKospiPrices().then((resp) => {
@@ -13,17 +15,39 @@ export default function Kospi(props) {
     });
   }, []);
 
-  if (kospi) {
-    const kospiChange =
-      (kospi[0].closePrice - kospi[currentPeriod + 1].closePrice) /
-      kospi[currentPeriod + 1].closePrice;
-    console.log(
-      "kos: ",
-      kospi[0].closePrice,
-      kospi[currentPeriod + 1].closePrice,
-      kospiChange
-    );
-  }
+  useEffect(() => {
+    if (kospi) {
+      const currentPrice = kospi[0].closePrice;
+      const pastPrice = kospi[currentPeriod + 1].closePrice;
+      const kospiChange = (
+        ((currentPrice - pastPrice) / pastPrice) *
+        100
+      ).toFixed(2);
 
-  return <div>Kospi</div>;
+      console.log(
+        "kos: ",
+        kospi[0].closePrice,
+        kospi[currentPeriod + 1].closePrice,
+        kospiChange
+      );
+
+      console.log("kospi[0]: ", kospiChange[0]);
+      if (kospiChange[0] === "-") {
+        console.log("minus : ", kospiChange.substring(1));
+        setMessage(kospiChange.substring(1) + "% 하락");
+        setStyle("text-blue-500");
+      } else {
+        setMessage(kospiChange + "% 상승");
+        setStyle("text-red-500");
+      }
+    }
+  }, [kospi, currentPeriod]);
+
+  return (
+    <div className="text-center m-4 text-md font-semibold">
+      <p>
+        같은기간, 코스피는 <span className={style}>{message}</span>했어요
+      </p>
+    </div>
+  );
 }
