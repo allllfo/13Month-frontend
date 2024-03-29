007f79cbb0ad9b03fb2fd541d656f6321b4fbcca
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-03-29 09:01:35 +0900

    feat: 연말정산 결과 페이지에 메인으로 버튼 팁

--- src/pages/preview/prevPreviewPage.jsx
+++ src/pages/preview/prevPreviewPage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import TopBackBar from "~/components/TopBackBar/TopBackBar";
+import BlueButton from "~/components/Button/BlueButton";
+import Tip from "~/components/Tip/Tip";
 
 export default function prevPreviewPage() {
   const userState = useSelector((state) => state.user13th);
@@ -11,6 +13,14 @@ export default function prevPreviewPage() {
       <TopBackBar />
 
       <p>이전 연말정산 결과 보기 페이지</p>
+
+      <div className="mt-16">
+        <Tip />
+      </div>
+
+      <div className="mt-8 flex justify-center">
+        <BlueButton text="메인으로" destination="/main" />
+      </div>
     </div>
   );
 }
