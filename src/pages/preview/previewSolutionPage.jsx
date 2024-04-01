d395e21c5b3b7ee6b7f2400f38227dd83f7beb40
youhyeoneee <khukyh2018@khu.ac.kr>
2024-04-01 13:30:43 +0900

    design: margin top 제거

--- src/pages/preview/previewSolutionPage.jsx
+++ src/pages/preview/previewSolutionPage.jsx
@@ -113,7 +113,7 @@ export default function PreviewSolutionPage() {
           아낄 수 있어요!
         </h1>
 
-        <div className="text-center mt-20">
+        <div className="text-center">
           <p className="mediumGreyText mb-1 text-base">
             공제를 추가해보세요. <br />
             추가할 때마다 아낄 수 있는 돈이 늘어나요.
