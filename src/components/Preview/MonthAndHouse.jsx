e5c2caf79e7b4da535c1bc9732c784961485b65e
allllfo <mirae4292@gmail.com>
2024-05-22 11:29:46 +0900

    refactor : useState 병합

--- src/components/Preview/MonthAndHouse.jsx
+++ src/components/Preview/MonthAndHouse.jsx
@@ -3,54 +3,77 @@ import { Accordion, Checkbox, Label, Radio, Card } from "flowbite-react";
 import houseImg from "~/assets/images/preview/house.png";
 import HouseProgressBar from "./HouseProgressBar";
 import { useSelector } from "react-redux";
+import {
+  LOAN_PERCENTAGE,
+  MAX_HOUSING_DEPOSIT,
+  MAX_LOAN_AMOUNT,
+} from "~/constants/Preview";
 
 const MonthAndHouse = ({ updateTotal, myData }) => {
-  const [state, setState] = React.useState({
-    monthly: false,
-    yearly: true,
+  const [userCheck, setUserCheck] = useState({
+    checkLoan: false, // 대출 보유 여부
+    checkMontly: false, // 월세 체크
+    checkYearly: false, // 전세 체크
   });
-
-  const [checkLoan, setCheckLoan] = useState(false);
-  const [checkMonthly, setCheckMonthly] = useState(false); // 월세 체크
-  const [checkYearly, setCheckYearly] = useState(false); // 전세 체크
+  const [result, setResult] = useState({
+    monthResult: 0, //월세 공제 금액
+    housingDepositResult: 0, //청약 공제 금액
+    loanResult: 0, // 전세 공제 금액
+    houseTotalResult: 0, // 주택 관련 총 공제 금액
+  });
   const [monthlyPay, setMonthlyPay] = useState(0); //월세 금액
-  const [monthResult, setMonthResult] = useState(0); //월세 공제 금액
-  const [housingDepositResult, setHousingDepositResult] = useState(0); // 청약 공제 금액
-  const [loanResult, setLoanResult] = useState(0); // 전세 공제 금액
-  const [houseTotalResult, setHouseTotalResult] = useState(0); // 주택 관련 총 공제 금액
   const [loan, setLoan] = useState(0); //대출
   const [housingDeposit, setHousingDeposit] = useState(0); // 저축연금
+
   const yearTax = useSelector((state) => state.yearTax);
 
   //월세 및 전세금 있는 지 여부 체크 버튼 핸들링
   const handleCheckLoanChange = (checkLoan) => {
-    console.log(checkLoan);
     if (checkLoan) {
-      setCheckLoan(false);
-      setMonthResult(0);
-      setHouseTotalResult(0);
-      updateTotal("house", 0);
-      updateTotal("month", 0);
+      setUserCheck((prev) => ({
+        ...prev,
+        checkLoan: false,
+      }));
+      setResult((prev) => ({
+        ...prev,
+        monthResult: 0,
+        houseTotalResult: 0,
+      }));
+
+      updateTotal({
+        house: 0,
+        momth: 0,
+      });
     } else {
-      setCheckLoan(true);
+      setUserCheck((prev) => ({
+        ...prev,
+        checkLoan: true,
+      }));
     }
   };
 
-  //월세/전세 두개중 하나 체크 버튼 핸들링
+  //월세,전세 두개중 하나 체크 버튼 핸들링
   const handleRadioChange = (event) => {
     if (event.target.value === "monthly") {
-      setCheckMonthly(event.target.checked);
-      setCheckYearly(false);
-    } else if (event.target.value === "yearly") {
-      setCheckYearly(event.target.checked);
-      setCheckMonthly(false); // 자가 선택 시 checkMonthly 상태를 false로 설정
+      setUserCheck((prev) => ({
+        ...prev,
+        checkMonthly: event.target.checked,
+        checkYearly: false,
+      }));
+    }
+    if (event.target.value === "yearly") {
+      setUserCheck((prev) => ({
+        // 자가 선택 시 checkMonthly 상태를 false로 설정
+        ...prev,
+        checkMonthly: event.target.checked,
+        checkMonthly: false,
+      }));
     }
   };
 
   //월세 입력 핸들링
   const handleMonthlyPay = (event) => {
-    const value = event.target.value;
-    setMonthlyPay(value);
+    setMonthlyPay(event.target.value);
   };
 
   // 월세 공제 결과 계산 함수
@@ -64,16 +87,15 @@ const MonthAndHouse = ({ updateTotal, myData }) => {
     } else {
       result = monthlyPay * 10000 * 12 * 0.15;
     }
-    setMonthResult(result);
-    updateTotal("month", result);
+    setResult((prev) => ({
+      ...prev,
+      monthResult: result,
+    }));
+    updateTotal({ month: result });
   };
 
   //주택 공제 결과 계산 함수
   const houseResultCalculate = () => {
-    const MAX_HOUSING_DEPOSIT = 2400000; // 주택청약공제
-    const MAX_LOAN_AMOUNT = 4000000;
-    const LOAN_PERCENTAGE = 0.4;
-
     let promiseAmount = Math.min(
       housingDeposit * 12 * LOAN_PERCENTAGE,
       MAX_HOUSING_DEPOSIT
@@ -82,19 +104,28 @@ const MonthAndHouse = ({ updateTotal, myData }) => {
       loan * LOAN_PERCENTAGE,
       MAX_LOAN_AMOUNT - promiseAmount
     ); //대출 공제금 => 400만원-주택청약 공제금 vs 대출공제금 중에 작은걸로 리턴
+
     let result = 0;
 
     if (yearTax.data.salary <= 70000000) {
       result = Math.min(promiseAmount + loanAmount, MAX_LOAN_AMOUNT);
-      setHousingDepositResult(promiseAmount);
-      console.log("house", housingDepositResult);
-      setLoanResult(Math.min(loanAmount, MAX_LOAN_AMOUNT));
+      setResult((prev) => ({
+        ...prev,
+        housingDepositResult: promiseAmount,
+        loanResult: Math.min(loanAmount, MAX_LOAN_AMOUNT),
+      }));
     } else {
       result = Math.min(loanAmount, MAX_LOAN_AMOUNT);
-      setLoanResult(Math.min(loanAmount, MAX_LOAN_AMOUNT));
+      setResult((prev) => ({
+        ...prev,
+        loanResult: Math.min(loanAmount, MAX_LOAN_AMOUNT),
+      }));
     }
-    setHouseTotalResult(result);
-    updateTotal("house", result);
+    setResult((prev) => ({
+      ...prev,
+      houseTotalResult: result,
+    }));
+    updateTotal({ house: result });
   };
 
   useEffect(() => {
@@ -111,15 +142,7 @@ const MonthAndHouse = ({ updateTotal, myData }) => {
     } else {
       houseResultCalculate();
     }
-  }, [
-    monthlyPay,
-    checkMonthly,
-    checkYearly,
-    housingDepositResult,
-    loanResult,
-    yearTax,
-    myData,
-  ]);
+  }, [monthlyPay, userCheck, result, yearTax, myData]);
 
   return (
     <>
